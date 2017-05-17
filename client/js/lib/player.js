0e89adb1075da95cd5f22bcb6258a80206694c5a
Javier <alu0100819441@ull.edu.es>
2017-05-17 10:44:02 +0100

    Implement player class in ecmas6

---
+++ client/js/lib/player.js
@@ -0,0 +1,67 @@
+(function(exports) {
+class Player {
+    constructor(pos) {
+        this.pos = pos.plus(new Vector(0, -0.5));
+        this.size = new Vector(0.8, 1.5);
+        this.speed = new Vector(0, 0);
+        this.type = "player";
+        this.score = 0;
+    }
+
+    moveX(step, level, keys) {
+        let playerXSpeed = 7;
+        this.speed.x = 0;
+        if (keys.left) 
+            this.speed.x -= playerXSpeed;
+        if (keys.right) 
+            this.speed.x += playerXSpeed;
+        var motion = new Vector(this.speed.x * step, 0);
+        var newPos = this.pos.plus(motion);
+        var obstacle = level.obstacleAt(newPos, this.size);
+        
+        if (obstacle)
+            level.playerTouched(obstacle);
+        else
+            this.pos = newPos;
+    }
+
+    moveY(step, level, keys) {
+        let gravity = 30;
+        let jumpSpeed = 17;
+        this.speed.y += step * gravity;
+        var motion = new Vector(0, this.speed.y * step);
+        var newPos = this.pos.plus(motion);
+        var obstacle = level.obstacleAt(newPos, this.size);
+        
+        if (obstacle) {
+            level.playerTouched(obstacle);
+            if (keys.up && this.speed.y > 0)
+                this.speed.y = -jumpSpeed;
+            else
+                this.speed.y = 0;
+        } else {
+            this.pos = newPos;
+        }
+    }
+
+    act(step, level, keys) {
+        this.moveX(step, level, keys);
+        this.moveY(step, level, keys);
+
+        var otherActor = level.actorAt(this);
+        if (otherActor)
+            if(level.playerTouched(otherActor.type, otherActor))
+                this.score+=50;
+                
+        // Losing animation
+        if (level.status == "lost") {
+            this.pos.y += step;
+            this.size.y -= step;
+        }
+    }
+}
+exports.Player = Player;
+
+})(this);
+
+
