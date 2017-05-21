f76f3876690aa3c6ce099fd39b9a2433ceb8a9a8
Javier <alu0100819441@ull.edu.es>
2017-05-21 19:23:17 +0100

    Add Level's width test

--- test/tests.js
+++ test/tests.js
@@ -9,14 +9,15 @@ var level="js/lib/level.js";
 var game ="js/lib/game.js";
 var levels="js/lib/game_levels.js";
 
-var mySuperLevel = ["           ",
+var customLevel = ["           ",
                     "@         o",
                     "xxx       x"]
 
+var aLevel = new Level(customLevel);
 describe("Level", function() {
-    describe("Size", function() {
-        it("Check the level's length", function() {
-            expect(mySuperLevel).to.have.lengthOf(3);
+    describe("Width", function() {
+        it("Check the level's width", function() {
+            expect(aLevel.width).to.equal(customLevel[0].length);
         });
     });    
 });
